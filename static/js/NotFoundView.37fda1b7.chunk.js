4d1e1765e4a26804c610f012bd7bf03806c444f2
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2020-12-30 13:02:45 +0200

    Updates

---
+++ static/js/NotFoundView.37fda1b7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[3],{67:function(e,r,t){"use strict";t.r(r);var s=t(1);r.default=function(e){var r=e.message;return Object(s.jsx)("div",{role:"alert",children:Object(s.jsxs)("p",{children:["Sorry, something went wrong. Error: ",r]})})}}}]);
+//# sourceMappingURL=NotFoundView.37fda1b7.chunk.js.map
